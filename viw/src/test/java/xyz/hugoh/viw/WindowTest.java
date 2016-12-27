6d5b450c916a2ca839b983e256912bbd3173f0a7
MergHQ <hugo.holm@live.com>
2016-12-27 22:06:44 +0200

    Add one test to window

---
+++ viw/src/test/java/xyz/hugoh/viw/WindowTest.java
@@ -0,0 +1,26 @@
+package xyz.hugoh.viw;
+
+import org.junit.Before;
+import org.junit.Test;
+import xyz.hugoh.viw.renderer.Window;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hugoh on 27/12/2016.
+ */
+
+public class WindowTest {
+
+    Window window;
+
+    @Before
+    public void setUp() {
+        window = new Window();
+    }
+
+    @Test
+    public void cameraIsNullOnInit() {  
+        assertTrue(window.getCamera() == null);
+    }
+}
